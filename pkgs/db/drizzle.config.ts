b1139009cab0bb7410ea5a9ab8afd1b08f7ed0a0
Victor Tantillo <v.tantillo3@gmail.com>
2024-03-17 10:57:36 -0600

    Have drizzle.config read from the .env file

--- pkgs/db/drizzle.config.ts
+++ pkgs/db/drizzle.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from 'drizzle-kit'
+import 'dotenv/config'
 
 export default {
   schema: './schema.ts',
   driver: 'better-sqlite',
   out: './drizzle',
   dbCredentials: {
-    url: './mini-db.db',
+    url: process.env.DB_PATH!,
   },
 } satisfies Config
